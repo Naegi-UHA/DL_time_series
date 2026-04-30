8e71ed3ab5d468d33bce9a6633a6b032afe451b4
will8459 <lliw.tri@gmail.com>
2026-04-30 17:03:11 +0200

    amélioration modele cnn

--- training/src/models/cnn.py
+++ training/src/models/cnn.py
@@ -6,10 +6,19 @@ def build_model(input_shape: tuple[int, ...], num_classes: int, config: dict) ->
     model = keras.Sequential(
         [
             keras.Input(shape=input_shape),
+
+
+            # Bloc Convolutif 1
             layers.Conv1D(config.get("filters_1", 16), config.get("kernel_1", 5), activation="relu", padding="same"),
             layers.MaxPooling1D(pool_size=2),
+            layers.Dropout(config.get("dropout_rate", 0.3)),
+
+            # Bloc Convolutif 2
             layers.Conv1D(config.get("filters_2", 32), config.get("kernel_2", 3), activation="relu", padding="same"),
             layers.GlobalAveragePooling1D(),
+            layers.Dropout(config.get("dropout_rate", 0.3)),
+
+            # Bloc de Classification
             layers.Dense(config.get("dense", 32), activation="relu"),
             layers.Dense(num_classes, activation="softmax"),
         ],
