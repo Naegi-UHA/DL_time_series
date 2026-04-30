f2abfea7bcb98ac89f49e6eaf192690a1e80a2ef
Naegi-UHA <lucas.runser@uha.fr>
2026-04-30 02:23:46 +0200

    version opti du mlp mise à jour

--- training/src/models/mlp.py
+++ training/src/models/mlp.py
@@ -6,11 +6,27 @@ def build_model(input_shape: tuple[int, ...], num_classes: int, config: dict) ->
     model = keras.Sequential(
         [
             keras.Input(shape=input_shape),
+
+            # Normalisation des entrées
+            layers.BatchNormalization(),
+
+            # couche 1
             layers.Dense(config.get("hidden_1", 64), activation="relu"),
-            layers.Dropout(config.get("dropout", 0.2)),
+            layers.BatchNormalization(),
+            layers.Dropout(config.get("dropout_1", 0.3)),
+
+            # couche 2
             layers.Dense(config.get("hidden_2", 32), activation="relu"),
+            layers.BatchNormalization(),
+            layers.Dropout(config.get("dropout_2", 0.3)),
+
+            # couche 3
+            layers.Dense(config.get("hidden_3", 16), activation="relu"),
+
+            # Sortie
             layers.Dense(num_classes, activation="softmax"),
         ],
         name="mlp",
     )
+
     return model
